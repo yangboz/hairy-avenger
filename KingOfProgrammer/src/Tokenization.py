f30c1557ea8a15668f89eeb7704328387215fd5c
unknown <yangboz@yzhou24.asiapacific.hpqcorp.net>
2013-06-13 20:40:22 +0800

    CHANGED:applied the cut_and_print() function;

--- KingOfProgrammer/src/Tokenization.py
+++ KingOfProgrammer/src/Tokenization.py
@@ -8,15 +8,17 @@
 #6.       能够去除文本中的噪音，例如url链接,@符号之后的人名清单，网络表情例如[嘻嘻]、[哈哈]，标点符号等。
 #7.       提交清单包括源代码以及编译好的可执行程序文件（如果需要编译），词库文件，分词结果文件以及说明文件（内容包括所使用的工具名称，工具版本，如何运行所提交的代码或程序）。
 #encoding=utf-8
-import jieba
+import sys
+print(sys.stdout.encoding)
 
 #Load file for testing
-print(open("assets/Test2TestData.txt").read())
-
-seg_list = jieba.cut("I come from China!", cut_all=False)
-print("Default mode:",",".join(seg_list))
-
+FILE_NAME = "assets/Test2TestData.txt"
+#print(open(FILE_NAME).read())
+open_read_file = open(FILE_NAME).read()
+    
 import jieba.posseg as pseg
-words = pseg.cut(u"我爱北京天安门")
-for w in words:
-    print(w.word, w.flag)
+def cut_and_print(text):
+    words =pseg.cut(text)
+    print ", ".join(["%s[%s]" % (word.word, word.flag) for word in words]).encode("utf-8")
+
+cut_and_print(open_read_file)
